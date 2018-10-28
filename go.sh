9736d9b06a70041889838bd511a77b3e685d5d5f
up-load <38355121+up-load@users.noreply.github.com>
2018-10-29 03:55:29 +0700

    Убраны 2 команды, требующие нажатия Enter

--- go.sh
+++ go.sh
@@ -9,7 +9,10 @@ if [[ $CONTINUE = "y" || $CONTINUE = "" ]]; then
 	sudo apt -y upgrade
 	sudo apt-get install -y software-properties-common
 	sudo apt-get install -y nano
-	sudo add-apt-repository ppa:nginx/stable
+#	sudo add-apt-repository ppa:nginx/stable
+	apt-key adv --keyserver keyserver.ubuntu.com --recv C300EE8C
+	echo "deb https://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list
+	echo "deb https://ppa.launchpad.net/ondrej/php/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list
 	sudo apt-get update
 	sudo apt-get install -y nginx
 	sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
@@ -19,8 +22,8 @@ if [[ $CONTINUE = "y" || $CONTINUE = "" ]]; then
 	sudo nginx -t
 	sudo systemctl reload nginx
 	sudo systemctl restart nginx
-	sudo add-apt-repository ppa:ondrej/php
-	sudo apt-get update
+#	sudo add-apt-repository ppa:ondrej/php
+#	sudo apt-get update
 	sudo apt install -y php7.2-cli php7.2-fpm php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip
 	sudo systemctl restart php7.2-fpm
 	sudo rm -rf lemp
